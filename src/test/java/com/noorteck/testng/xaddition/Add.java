6325f0fdb0ebb5d64d904c9d10a26ef293b4ed01
dennn <dennn@DESKTOP-2N6ADQC>
2020-07-05 16:00:27 -0400

    Add

---
+++ src/test/java/com/noorteck/testng/xaddition/Add.java
@@ -0,0 +1,45 @@
+package com.noorteck.testng.xaddition;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class Add {
+ 
+  @BeforeMethod
+  public void beforeMethod() {
+	  
+	  System.out.println("ADD Class: @BeforeMethod");
+  }
+  
+  @Test
+  public void addTwoNum() {
+	  
+	  int n1=10;
+	  int n2=20;
+	  
+	  int result=n1+n2;
+	  System.out.println(n1 + "+" + n2 + "=" + result);
+	  
+	  
+  }
+  
+  @Test
+  public void addThreeNum() {
+	  
+	  int n1=10;
+	  int n2=20;
+	  int n3=30;
+	  
+	  int result=n1+n2+n3;
+	  System.out.println(n1 + "+" + n2 +" + " + n3 + "=" + result);
+	  
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  
+	  System.out.println("Add Class:@AfterMethod");
+  }
+
+}
